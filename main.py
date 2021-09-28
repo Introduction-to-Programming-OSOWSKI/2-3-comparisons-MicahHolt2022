a8303eb409b1521bb8655723b1cf3f76339e37b0
MicahHolt2022 <mccormickprophet@gmail.com>
2021-09-28 09:23:59 -0500

    First Attempt

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
 #WRITE YOUR CODE IN THIS FILE
+
+def greaterThan(a,b):
+    if a > b:
+        return True
+    else:
+        return False
+def lessThan(a,b):
+    if a < b:
+        return True
+    else:
+        return False
+def equalTo(a,b):
+    if a == b:
+        return True
+    else:
+        return False
+def greaterOrEqual(a,b):
+    if a > b:
+        return True
+    elif a == b:
+        return True
+    else:
+        return False
+def lessOrEqual(a,b):
+    if a < b:
+        return True
+    elif a == b:
+        return True
+    else:
+        return False
